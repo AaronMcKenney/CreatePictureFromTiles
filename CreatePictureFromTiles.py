293613799174d8009708969154cbea249d951929
Aaron McKenney <aaronmckenney@gmail.com>
2018-06-11 01:57:30 -0700

    Added ExperimentalCompare. It doesn't work well

--- CreatePictureFromTiles.py
+++ CreatePictureFromTiles.py
@@ -18,17 +18,27 @@ RIGHT = 1
 BOT = 2
 LEFT = 3
 
+R = 0
+G = 1
+B = 2
+
+Y = 0
+U = 1
+V = 2
+
 g_do_log = False
 g_log_file = None
+g_err_occurred = False
 
 def RGB2YUV(rgb):
 	#Takes a single pixel in rgb (3-tuple) and converts it to yuv (3-tuple)
 	rgb_uniform = (rgb[0]/255.0, rgb[1]/255.0, rgb[2]/255.0)
-	#HDTV with BT.709 conversion from https://en.wikipedia.org/wiki/YUV#Converting_between_Y%E2%80%B2UV_and_RGB
+	#Calculate YUV using "HDTV with BT.709 conversion" algorithm from 
+	#https://en.wikipedia.org/wiki/YUV#Converting_between_Y%E2%80%B2UV_and_RGB
 	yuv = (
-		0.2126*rgb_uniform[0]   + 0.7152*rgb_uniform[1]  + 0.0722*rgb_uniform[2],
-		-0.09991*rgb_uniform[0] - 0.33609*rgb_uniform[1] + 0.436*rgb_uniform[2],
-		0.615*rgb_uniform[0]    - 0.55861*rgb_uniform[1]  - 0.05639*rgb_uniform[2]
+		0.2126*rgb_uniform[R]   + 0.7152*rgb_uniform[G]  + 0.0722*rgb_uniform[B],
+		-0.09991*rgb_uniform[R] - 0.33609*rgb_uniform[G] + 0.436*rgb_uniform[B],
+		0.615*rgb_uniform[R]    - 0.55861*rgb_uniform[G]  - 0.05639*rgb_uniform[B]
 	)
 	
 	return yuv
@@ -40,6 +50,42 @@ def RGBList2YUVTuple(rgb_list):
 		yuv_list.append(RGB2YUV(rgb))
 	
 	return tuple(yuv_list)
+
+def ExperimentalCompare(x_list, y_list):
+	#if len(x_list) < 4 or len(y_list) < 4 or len(x_list) is not len(y_list) or len(x_list) % 4 is not 0:
+	#	return x_list == y_list
+	#
+	##Compare pixels four at a time and compare the differences.
+	##Return False if the difference of the averages of these 4 pixels is too large
+	#for i in range(len(x_list)//4):
+	#	#People's eyes are more sensitive to luma changes, so there must be a lower tolerance for differences
+	#	y_avg_x = (x_list[i + 0][Y] + x_list[i + 1][Y] + x_list[i + 2][Y] + x_list[i + 3][Y])/4.0
+	#	y_avg_y = (y_list[i + 0][Y] + y_list[i + 1][Y] + y_list[i + 2][Y] + y_list[i + 3][Y])/4.0
+	#	if abs(y_avg_x - y_avg_y) > 0.07:
+	#		return False
+	#		
+	#	u_avg_x = (x_list[i + 0][U] + x_list[i + 1][U] + x_list[i + 2][U] + x_list[i + 3][U])/4.0
+	#	u_avg_y = (y_list[i + 0][U] + y_list[i + 1][U] + y_list[i + 2][U] + y_list[i + 3][U])/4.0
+	#	if abs(u_avg_x - u_avg_y) > 0.15:
+	#		return False
+	#	
+	#	v_avg_x = (x_list[i + 0][V] + x_list[i + 1][V] + x_list[i + 2][V] + x_list[i + 3][V])/4.0
+	#	v_avg_y = (y_list[i + 0][V] + y_list[i + 1][V] + y_list[i + 2][V] + y_list[i + 3][V])/4.0
+	#	if abs(v_avg_x - v_avg_y) > 0.15:
+	#		return False
+	#
+	#return True
+	
+	for i in range(len(x_list)):
+		#People's eyes are more sensitive to luma changes, so there is a lower tolerance for differences
+		if abs(x_list[i][Y] - y_list[i][Y]) > 0.07:
+			return False
+		if abs(x_list[i][U] - y_list[i][U]) > 0.15:
+			return False
+		if abs(x_list[i][V] - y_list[i][V]) > 0.15:
+			return False
+	
+	return True
 	
 class Tile:
 	def __init__(self, im):
@@ -57,9 +103,12 @@ class Tile:
 		
 	def CompareBoundaries(self, dir, boundaries):
 		if boundaries == []:
-			return True #Boundaries is erroneous and anything goes.
+			return True #Either at the edge of the frame or boundaries is erroneous and anything goes.
 		elif len(boundaries) == 1:
-			return self.boundaries[dir] == boundaries[0]
+			#New method (which uses too much fudging for my liking):
+			return ExperimentalCompare(self.boundaries[dir], boundaries[0])
+			#Old method: only return true if the pixels have an exact match.
+			#return self.boundaries[dir] == boundaries[0]
 		else:
 			return self.boundaries[dir] in boundaries
 
@@ -129,13 +178,14 @@ def SetupLogging(do_log):
 		g_log_file = open(LOG_NAME, 'w')
 		
 def Log(level, statement):
-	global g_do_log, g_log_file
+	global g_do_log, g_log_file, g_err_occurred
 	
 	log_line = level + ': ' + statement + '\n'
 	if g_do_log:
 		g_log_file.write(log_line)
 	elif level == ERR:
 		print(log_line)
+		g_err_occurred = True
 
 def CloseLog():
 	global g_do_log, g_log_file
@@ -167,6 +217,17 @@ def CreatePicture(out_image_name, tile_grid, tile_map, frame_width, frame_height
 
 	new_im.save(out_image_name)
 
+def FastestProcessTileGrid(tile_grid, tile_map, frame_width, frame_height):
+	if tile_grid == []:
+		return []
+		
+	#Fill tile grid from left to right, top to bottom.
+	for i in range(frame_height):
+		for j in range(frame_width):
+			tile_grid[i][j] = random.choice(tile_grid[i][j])
+	
+	return tile_grid
+	
 def FastProcessTileGrid(tile_grid, tile_map, frame_width, frame_height):
 	if tile_grid == []:
 		return []
@@ -246,6 +307,9 @@ def ProcessTileGrid(tile_grid, tile_map, frame_width, frame_height):
 				open_set.add((x - 1, y))
 			if x < frame_width - 1 and tile_grid[y][x + 1] != []:
 				open_set.add((x + 1, y))
+	
+	print('  Finished Pruning Impossibilities.')
+	sys.stdout.flush()
 	
 	#Fill tile grid from left to right, top to bottom.
 	propogate_error = False
@@ -380,16 +444,20 @@ def GetTilesFromImages(im_list):
 def GetImagesFromPath(path, add_im):
 	im_list = []
 	im_size = None
-
+	
 	if not os.path.isdir(path):
 		Log(ERR, 'Input path (' + path + ') does not point to a directory')
 		return []
-
+		
 	files = glob.glob(os.path.join(path, '*'))
 	
+	print('Loading Images:')
+	files_loaded = 0
+	percent_done = 10.0
 	for file in files:
 		if not os.path.isfile(file):
 			Log(WARN, 'Could not get image information from ' + file + '. File recursion not supported.')
+			files_loaded += 1
 			continue
 		
 		try:
@@ -428,9 +496,17 @@ def GetImagesFromPath(path, add_im):
 			#Presumably the image files are resting in a directory with other non-image files.
 			Log(WARN, str(err))
 	
+		files_loaded += 1
+		if (files_loaded / len(files))*100.0 >= percent_done:
+			print('  ' + str(percent_done) + '% of images have been loaded.')
+			sys.stdout.flush()
+			percent_done += 10
+	
+	#TODO: This call is really slow and completely useless when there's a lot of high quality tiles.
+	#  Add a flag to enable/disable duplicate image deletion, and have it disabled by default.
 	#Note: Normally would delete duplicates by having images be a set and avoid a function call, 
 	#but that won't work here, as each image contains some file object member.
-	im_list = DeleteDuplicateImages(im_list)
+	#im_list = DeleteDuplicateImages(im_list)
 	
 	if im_list == []:
 		Log(ERR, 'Could not find any image files in ' + path)
@@ -492,6 +568,7 @@ def CloseImages(im_list):
 		im.close()
 
 def Main():
+	global g_err_occurred
 	args = ParseCommandLineArgs()
 	
 	SetupLogging(args.log)
@@ -501,6 +578,9 @@ def Main():
 	
 	im_list = GetImagesFromPath(args.path, args.add_im)
 	tile_map = GetTilesFromImages(im_list)
+	if not g_err_occurred:
+		print('Tiles have been created.\nCreating Tile Grid.')
+		sys.stdout.flush()
 	
 	if args.grid == '':
 		(frame_width, frame_height) = Get2TupleFromStr(args.size)
@@ -509,15 +589,27 @@ def Main():
 		grid_path = os.path.join(args.path, args.grid)
 		(tile_grid, frame_width, frame_height) = GetTileGridFromFile(grid_path, tile_map)
 	
-	if args.speed_mode:
-		FastProcessTileGrid(tile_grid, tile_map, frame_width, frame_height)
-	else:
-		ProcessTileGrid(tile_grid, tile_map, frame_width, frame_height)
+	if not g_err_occurred:
+		print('Created Tile Grid.\nProcessing Tile Grid.')
+		sys.stdout.flush()
 	
+	FastestProcessTileGrid(tile_grid, tile_map, frame_width, frame_height)#To remove??
+	#UNDO
+	#if args.speed_mode:
+	#	FastProcessTileGrid(tile_grid, tile_map, frame_width, frame_height)
+	#else:
+	#	ProcessTileGrid(tile_grid, tile_map, frame_width, frame_height)
+	
+	if not g_err_occurred:
+		print('Processing has finished. Creating picture')
+		sys.stdout.flush()
 	CreatePicture(args.out, tile_grid, tile_map, frame_width, frame_height)
 	
 	CloseImages(im_list)
 	CloseLog()
+	
+	if not g_err_occurred:
+		print('DONE')
 	
 if __name__ == "__main__":
 	Main()
